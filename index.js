bdd25d856b76d2afe020b655886405ff38ac2281
yaswanth <82691830+yaswanthkumarch@users.noreply.github.com>
2024-10-22 08:29:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const Book = require('./models/Book');
+const Book = require('./Book');
 const app = express();
 
 app.use(cors());
